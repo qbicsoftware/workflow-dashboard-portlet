c6a9cc4d38c8e9887c4fb1241b924393008e2ea8
KochTobi <tobias.koch@qbic.uni-tuebingen.de>
2019-12-20 15:30:36 +0100

    add empty tests for ListWorkflowRuns use case

---
+++ src/test/groovy/life/qbic/portal/domain/ListWorkflowRunsSpec.groovy
@@ -0,0 +1,26 @@
+package life.qbic.portal.domain
+
+import spock.lang.Specification
+
+class ListWorkflowRunsSpec extends Specification{
+
+    def "retrieve all RunInfo of workflow runs as list"() {
+        assert false
+    }
+
+    def "filter a list of workflow RunInfo by submitter"() {
+        assert false
+    }
+
+    def "retrieve submitter ID from workflow service for RunId"() {
+        assert false
+    }
+
+    def "validating RunId"() {
+        assert false
+    }
+
+    def "generate response"() {
+        assert false
+    }
+}
