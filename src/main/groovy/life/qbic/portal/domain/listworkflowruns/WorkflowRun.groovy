a1e056482cc027d6b9ee2551f6c2d9722f902bb4
KochTobi <tobias.koch@qbic.uni-tuebingen.de>
2020-01-02 16:47:48 +0100

    modify WorkflowRun object add field runningTaskName add field currentRunStatus

--- src/main/groovy/life/qbic/portal/domain/listworkflowruns/WorkflowRun.groovy
+++ src/main/groovy/life/qbic/portal/domain/listworkflowruns/WorkflowRun.groovy
@@ -2,7 +2,9 @@ package life.qbic.portal.domain.listworkflowruns
 
 class WorkflowRun {
     String runName
+    String runningTaskName
     Date startTime
+    def currentRunStatus
     int submittedCount
     int runningCount
     int completedCount
